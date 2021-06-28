af8ef31141a3bd919b66759b548d1aefa3504413
markb500 <81427572+markb500@users.noreply.github.com>
2021-06-28 18:15:36 +0100

    Add files via upload

--- pressure_module.js
+++ pressure_module.js
@@ -1,4 +1,5 @@
-var prevsum = 0, p1, p2, v1, v2, t1, t2;
+var prevsum = 0, p1, p2, v1, v2, t1, t2, h, hman, hbar, rho, rhoman, rhobar, rho1, rho2, g;
+var patmos, pabs, pgauge, F, a, rhofresh, rhosea;
 function pressure() {
     var sum;
     document.getElementById("myCanvas");
@@ -13,9 +14,9 @@ function pressure() {
         window.open("images/Sci Bk2 Statics v1.10.pdf#page=49", "_blank")
     }
     do {
-        sum = 5//rndgen(1, 15, 0, 1, -1);
+        sum = rndgen(1, 15, 0, 1, -1);
     } while(sum === prevsum)
-    //prevsum = sum;
+    prevsum = sum;
     switch(sum) {
         case 1:
             v1 = rndgen(2.5, 7, 1, 0.1, -1);
@@ -99,23 +100,243 @@ function pressure() {
                     dp((p2 * v2 * (t1 + 273)) / ((t2 + 273) * p1), 2, 1) + "\\ in^3\\ (1\\ dp)}}\\end{aligned}$$";
             break;
         case 5:
-            // p1 = Int((151 - 80) * Rnd() + 80)
-            // t1 = Round((16 - 8) * Rnd() + 8, 1)
-            // t2 = Round((41 - 30) * Rnd() + 30, 1)
-            //     ssumarray(2) = """. A tyre is inflated so it has an absolute pressure of """ & p1 & """ psi at """ & _
-            //                                                         t1 & """ """ & "^O " & """C. What is the new"""
-            //     ssumarray(3) = """     absolute pressure, to an accuracy of 1 decimal place, if the tyre is heat soaked at """ & _
-            //                                                                                     t2 & """ """ & "^O " & """C,"""
-            //     ssumarray(4) = """     assuming the volume remains constant?"""
-            //     ssumarray(7) = " P_1 " & """ = """ & p1 & """ psi"""
-            //     ssumarray(8) = " T_1 " & """ = """ & t1 & """ + 273 = """ & t1 + 273 & """ K"""
-            //     ssumarray(9) = " T_2 " & """ = """ & t2 & """ + 273 = """ & t2 + 273 & """ K"""
-            //     ssumarray(10) = " P_2 " & """ = ?"""
-            //     ssumarray(11) = """ """
-            //     ssumarray(12) = "\frac{ P_1 }{ T_1 }=\frac{ P_2 }{ T_2 } " & """     Note: volume remains constant."""
-            //     ssumarray(13) = " P_2 = \frac{ P_1 T_2 }{ T_1 }=\frac{" & p1 & Chr(215) & t2 + 273 & "}{" & t1 + 273 & "}=" & _
-            //                                         Format((p1 * (t2 + 273)) / (t1 + 273), "0.0") & """ psi (1 dp)"""
-            
+            p1 = rndgen(80, 150, 0, 1, -1);
+            t1 = rndgen(8, 15, 1, 0.1, -1);
+            t2 = rndgen(30, 40, 1, 0.1, -1);
+            sumq += "A tyre is inflated so it has an absolute pressure of " + p1 + " psi at " + t1 + 
+                    " <sup>O</sup>C. What is the new absolute pressure, to an accuracy of 1 decimal place, " + 
+                    "if the tyre is heat soaked at " + t2 + " <sup>O</sup>C, assuming the volume remains constant?";
+            suma += "\\(P_1=" + p1 + "\\ psi\\\\\\)";
+            suma += "\\(T_1=" + t1 + "+273=" + (t1 + 273) + "\\ K\\\\\\)";
+            suma += "\\(T_2=" + t2 + "+273=" + (t2 + 273) + "\\ K\\\\\\)";
+            suma += "\\(P_2=\\ ?\\ psi\\\\\\)";
+            suma += "$$\\begin{aligned}\\frac{P_1}{T_1}&=\\frac{P_2}{T_2}\\qquad\\qquad " + 
+                    "Note:\\ volume\\ remains\\ constant.\\\\[5pt]";
+            suma += "P_2&=\\frac{P_1 T_2}{T_1}=\\frac{" + p1 + "\\times" + (t2 + 273) + "}{" + (t1 + 273) + 
+                    "}=\\underline{\\mathbf{" + dp((p1 * (t2 + 273)) / (t1 + 273), 3, 1) + 
+                    "\\ psi\\ (1\\ dp)}}\\end{aligned}$$";
+            break;
+        case 6:
+            v1 = rndgen(800, 1200, 0, 1, -1);
+            t1 = rndgen(20, 28, 1, 0.1, -1);
+            t2 = rndgen(33, 40, 1, 0.1, -1);
+            sumq += "A hot air balloon contains " + thouSep(v1, " ") + " m<sup>3</sup> of air at " + t1 + 
+                    " <sup>O</sup>C. What volume will it occupy at " + t2 + 
+                    " <sup>O</sup>C, assuming the pressure remains constant? Round the answer to 1 decimal place.";
+            suma += "\\(V_1=" + thouSep(v1, " ") + "\\ m^3\\\\\\)";
+            suma += "\\(T_1=" + t1 + "+273=" + (t1 + 273) + "\\ ^OC\\\\\\)";
+            suma += "\\(T_2=" + t2 + "+273=" + (t2 + 273) + "\\ ^OC\\\\\\)";
+            suma += "\\(V_2=\\ ?\\ m^3\\\\\\)";
+            suma += "$$\\begin{aligned}\\frac{V_1}{T_1}&=\\frac{V_2}{T_2}\\qquad\\qquad " + 
+                    "Note:\\ pressure\\ remains\\ constant.\\\\[5pt]";
+            suma += "V_2&=\\frac{V_1 T_2}{T_1}=\\frac{" + thouSep(v1, "\\ ") + "\\times" + (t2 + 273) + "}{" + 
+                    (t1 + 273) + "}=\\underline{\\mathbf{" + 
+                    thouSep(dp((v1 * (t2 + 273)) / (t1 + 273), 3, 1), "\\ ") + "\\ m^3\\ (1\\ dp)}}\\end{aligned}$$";
+            break;
+        case 7:
+            v1 = rndgen(4, 10, 0, 1, -1);
+            p1 = rndgen(80, 250, 1, 0.1, -1);
+            p2 = rndgen(1100, 1300, 1, 0.1, -1);
+            sumq += "An air compressor receives " + v1 + " m<sup>3</sup> of air at a pressure of " + p1 + 
+                    " kN/m<sup>2</sup> absolute and compresses it to a pressure of " + thouSep(p2, " ") + 
+                    " kN/m<sup>2</sup> absolute. Calculate the final volume of air, rounded to 2 decimal places, " + 
+                    "assuming no change in temperature.";
+            suma += "\\(V_1=" + v1 + "\\ m^3\\\\\\)";
+            suma += "\\(P_1=" + p1 + "\\times10^3\\ N/m^2\\\\\\)";
+            suma += "\\(P_2=" + thouSep(p2, "\\ ") + "\\times10^3\\ N/m^2\\\\\\)";
+            suma += "\\(V_2=\\ ?\\ m^3\\\\\\)";
+            suma += "$$\\begin{aligned}P_1 V_1&=P_2 V_2\\qquad\\qquad " + 
+                    "Note:\\ temperature\\ remains\\ constant.\\\\[5pt]";
+            suma += "V_2&=\\frac{P_1 V_1}{P_2}=\\frac{" + p1 + "\\times10^3\\times" + v1 + "}{" + p2 + 
+                    "\\times10^3}=\\underline{\\mathbf{" + dp((p1 * 1000 * v1) / (p2 * 1000), 4, 2) + 
+                    "\\ m^3\\ (2\\ dp)}}\\end{aligned}$$";
+            break;
+        case 8:
+            v1 = rndgen(2, 5, 1, 0.1, -1);
+            t1 = rndgen(40, 60, 0, 1, -1);
+            v2 = v1 * 2;
+            sumq += "If " + v1 + " m<sup>3</sup> of gas at " + t1 + 
+                    " <sup>O</sup>C is heated at a constant pressure until its volume is doubled, " + 
+                    "what will be the final temperature, in <sup>O</sup>C?";
+            suma += "\\(V_1=" + v1 + "\\ m^3\\\\\\)";
+            suma += "\\(T_1=" + t1 + "+273=" + (t1 + 273) + "\\ K\\\\\\)";
+            suma += "\\(V_2=V_1\\times2=" + v2 + "\\ m^3\\\\\\)";
+            suma += "\\(T_2=\\ ?\\ ^OC\\\\\\)";
+            suma += "$$\\begin{aligned}\\frac{V_1}{T_1}&=\\frac{V_2}{T_2}\\\\[5pt]";
+            suma += "T_2&=\\frac{T_1 V_2}{V_1}=\\frac{" + (t1 + 273) + "\\times" + v2 + "}{" + v1 + "}=" + 
+                    dp(((t1 + 273) * v2) / v1, 2, -1) + "\\ K\\\\[5pt]";
+            suma += "&=" + dp(((t1 + 273) * v2) / v1, 2, -1) + "-273=\\underline{\\mathbf{" + 
+                    (dp(((t1 + 273) * v2) / v1, 2, -1) - 273) + "\\ ^OC}}\\end{aligned}$$";
+            break;
+        case 9:
+            v1 = rndgen(5, 20, 0, 1, -1);
+            p1 = rndgen(100.5, 150, 1, 0.1, -1);
+            t1 = rndgen(10, 30,0, 1, -1);
+            p2 = rndgen(400.5, 500, 1, 0.1, -1);
+            t2 = rndgen(100, 140, 0, 1, -1);
+            sumq += "If " + v1 + " m<sup>3</sup> of air is compressed from initial conditions of " + p1 + 
+                    " kN/m<sup>2</sup> absolute and " + t1 + " <sup>O</sup>C to final conditions of " + p2 + 
+                    " kN/m<sup>2</sup> absolute and " + t2 + 
+                    " <sup>O</sup>C, calculate the final volume of the air, to an accuracy of 2 decimal places.";
+            suma += "\\(V_1=" + v1 + "\\ m^3\\\\\\)";
+            suma += "\\(P_1=" + p1 + "\\times10^3\\ N/m^2\\\\\\)";
+            suma += "\\(T_1=" + t1 + "+273=" + (t1 + 273) + "\\ K\\\\\\)";
+            suma += "\\(P_2=" + p2 + "\\times10^3\\ N/m^2\\\\\\)";
+            suma += "\\(T_2=" + t2 + "+273=" + (t2 + 273) + "\\ K\\\\\\)";
+            suma += "\\(V_2=\\ ?\\ m^3\\\\\\)";
+            suma += "$$\\begin{aligned}\\frac{P_1 V_1}{T_1}&=\\frac{P_2 V_2}{T_2}\\\\[5pt]";
+            suma += "V_2&=\\frac{P_1 V_1 T_2}{T_1 P_2}=\\frac{" + p1 + "\\times10^3\\times" + v1 + 
+                    "\\times" + (t2 + 273) + "}{" + (t1 + 273) + "\\times" + p2 + 
+                    "\\times10^3}=\\underline{\\mathbf{" + 
+                    dp((p1 * 1000 * v1 * (t2 + 273)) / ((t1 + 273) * p2 * 1000), 4, 2) + 
+                    "\\ m^3\\ (2\\ dp)}}\\end{aligned}$$"; 
+            break;
+        case 10:
+            p1 = rndgen(0.75, 1.20, 2, 0.1, -1);
+            t1 = rndgen(15, 30, 0, 1, -1);
+            v1 = 4;
+            v2 = 1;
+            sumq += "Air is compressed in a cylinder from a pressure of " + p1 + " bar and a temperature of " + 
+                    t1 + " <sup>O</sup>C to one quarter of its original volume at a constant temperature.<BR>"
+            sumq += "i. What is the resulting pressure?<BR>";
+            sumq += "ii. The air is then heated at a constant pressure until it occupies its original volume. " + 
+                    "Calculate the final temperature in <sup>O</sup>C.";
+            suma += "\\(P_1=" + p1 + "\\ bar\\\\\\)";
+            suma += "\\(T_1=" + t1 + "+273=" + (t1 + 273) + "\\ K\\\\\\)";
+            suma += "\\(V_1=4m^3\\\\\\)";
+            suma += "\\(V_2=1m^3\\\\\\)";
+            suma += "\\(P_2=\\ ?\\ bar\\\\\\)";
+            suma += "\\(T_2=\\ ?\\ ^OC\\\\\\)";
+            suma += "$$\\begin{aligned}i.\\ \\ P_1 V_1&=P_2 V_2\\qquad\\qquad " + 
+                    "Note:\\ temperature\\ remains\\ constant.\\\\[5pt]";
+            suma += "P_2&=\\frac{P_1 V_1}{V_2}=\\frac{" + p1 + "\\times" + v1 + "}{" + v2 + "}=" + 
+                    dp(p1 * v1 / v2, 3, -1) + "\\ bar\\\\[5pt]";
+            suma += "ii.\\ \\frac{V_1}{T_1}&=\\frac{V_2}{T_2}\\qquad\\qquad " + 
+                    "Note:\\ pressure\\ remains\\ constant.\\\\[5pt]";
+            suma += "T_2&=\\frac{V_2 T_1}{V_1}=\\frac{" + v1 + "\\times" + (t1 + 273) + "}{" + v2 + "}=" + 
+                    thouSep(dp(v1 * (t1 + 273) / v2, 3, -1), "\\ ") + "\\ K\\\\[5pt]";
+            suma += "&=" + thouSep(dp(v1 * (t1 + 273) / v2, 3, -1), "\\ ") + "-273=\\underline{\\mathbf{" + 
+                    (dp(v1 * (t1 + 273) / v2, 3, -1) - 273) + "^OC}}\\end{aligned}$$";
+            break;
+        case 11:
+            document.getElementById("noteslink").onclick = function() {
+                window.open("images/Sci Bk2 Statics v1.10.pdf#page=64", "_blank")
+            }
+            h = rndgen(1.85, 2.85, 2, 0.01, -1);
+            rho = rndgen(0.71, 0.90, 2, 0.01, -1);
+            g = 9.81;
+            sumq += "A manometer reads " + h + " m. If the working fluid has a relative density of " + rho + 
+                    ", calculate the gauge pressure being indicated, both in N/m<sup>2</sup> and bar, " + 
+                    "each rounded to 2 decimal places.";
+            suma += "\\(h=" + h + "\\ m\\\\\\)";
+            suma += "\\(\\rho=" + rho + "\\times1\\ 000=" + dp(rho * 1000, 2, -1) + "\\ kg/m^3\\\\\\)";
+            suma += "\\(P=\\ ?\\\\\\)";
+            suma += "$$\\begin{aligned}P&=\\rho gh=" + thouSep(rho, "\\ ") + "\\times" + g + "\\times" + 
+                    h + "=\\underline{\\mathbf{" + thouSep(dp(rho * 1000 * g * h, 4, 2), "\\ ") + 
+                    "\\ N/m^2\\ (2\\ dp)}}\\\\[5pt]";
+            suma += "&=\\frac{" + dp(rho * 1000 * g * h, 4, 2) + "}{" + thouSep(100000, "\\ ") + 
+                    "}=\\underline{\\mathbf{" + dp(dp(rho * 1000 * g * h, 4, 2) / 100000, 4, 2) + 
+                    "\\ bar\\ (2\\ dp)}}\\end{aligned}$$";
+            break;
+        case 12:
+            document.getElementById("noteslink").onclick = function() {
+                window.open("images/Sci Bk2 Statics v1.10.pdf#page=64", "_blank")
+            }
+            patmos = rndgen(99.8, 102, 1, 0.1, -1);
+            rho = 1.02;
+            h = rndgen(8, 25, 0, 1, -1);
+            g = 9.81;
+            pabs = dp((rho * 1000 * g * h) + (patmos * 1000), 3, -1);
+            sumq += "Given atmospheric pressure as " + patmos + 
+                    " kN/m<sup>2</sup> and that 'sea water' has a relative density of " + rho + 
+                    ", calculate the absolute pressure on the hull of a submarine at a depth of " + h + 
+                    " m, giving your answer in N/m<sup>2</sup> and bar, each to an accuracy of 1 decimal place.";
+            suma += "\\(P_{ATMOS}=" + patmos + "\\times10^3\\ N/m^2\\\\\\)";
+            suma += "\\(\\rho=" + rho + "\\times1\\ 000=" + thouSep((rho * 1000), "\\ ") + "\\ kg/m^3\\\\\\)";
+            suma += "\\(h=" + h + "\\ m\\\\\\)";
+            suma += "\\(P_{ABS}=\\ ?\\ N/m^2\\\\\\)";
+            suma += "$$\\begin{aligned}P_{GUAGE}&=\\rho gh=" + thouSep((rho * 1000), "\\ ") + "\\times" + g + 
+                    "\\times" + h + "=" + thouSep(dp(rho * 1000 * g * h, 4, -1), "\\ ") + "\\ N/m^2\\\\[5pt]";
+            suma += "P_{ABS}&=P_{GUAGE}+ P_{ATMOS}=" + thouSep(dp(rho * 1000 * g * h, 4, -1), "\\ ") + 
+                    "+" + patmos + "\\times10^3=\\underline{\\mathbf{" + thouSep(pabs, "\\ ") + 
+                    "\\ N/m^2\\ (1\\ dp)}}\\\\[5pt]";
+            suma += "&=\\frac{" + thouSep(pabs, "\\ ") + "}{100\\ 000}=\\underline{\\mathbf{" + 
+                    dp(pabs / 100000, 3, 1) + "\\ bar\\ (1\\ dp)}}\\end{aligned}$$";
+            break;
+        case 13:
+            document.getElementById("noteslink").onclick = function() {
+                window.open("images/Sci Bk2 Statics v1.10.pdf#page=60", "_blank")
+            }
+            hman = rndgen(205, 305, 0, 1, -1);
+            rhoman = 1200;
+            hbar = rndgen(735, 755, 0, 1, -1);
+            rhobar = 13600;
+            g = 9.81;
+            pguage = dp(rhoman * g * (hman / 1000), 3, -1);
+            patmos = dp(rhobar * g * (hbar / 1000), 3, -1);
+            sumq += "A manometer on a gas pipe reads " + hman + " mm of oil (oil density = " + 
+                    thouSep(rhoman, " ") + " kg/m<sup>3</sup>). A barometer alongside the pipe reads " + 
+                    hbar + " mmHg (Hg density = " + thouSep(rhobar, " ") + 
+                    " kg/m<sup>3</sup>). Calculate the absolute pressure of the gas in the pipe, " + 
+                    "in N/m<sup>2</sup> rounded to 2 decimal places.";
+            suma += "\\(h_{MAN}=" + hman + "\\times10^{-3}\\ m\\\\\\)";
+            suma += "\\(\\rho_{MAN}=" + thouSep(rhoman, "\\ ") + "\\ kg/m^3\\\\\\)";
+            suma += "\\(h_{BAR}=" + hbar + "\\times10^{-3}\\ m\\\\\\)";
+            suma += "\\(\\rho_{BAR}=" + thouSep(rhobar, "\\ ") + "\\ kg/m^3\\\\\\)";
+            suma += "\\(p_{ABS}=\\ ?\\ N/m^2\\\\\\)";
+            suma += "$$\\begin{aligned}P_{GUAGE}&=\\rho_{MAN}\\ g\\ h_{MAN}=" + thouSep(rhoman, "\\ ") + 
+                    "\\times" + g + "\\times" + hman + "\\times10^{-3}=" + thouSep(pguage, "\\ ") + 
+                    "\\ N/m^2\\\\[5pt]";
+            suma += "P_{ATMOS}&=\\rho_{BAR}\\ g\\ h_{BAR}=" + thouSep(rhobar, "\\ ") + "\\times" + g + 
+                    "\\times" + hbar + "\\times10^{-3}=" + thouSep(patmos, "\\ ") + "\\ N/m^2\\\\[5pt]";
+            suma += "P_{ABS}&=P_{GUAGE}+ P_{ATMOS}=" + thouSep(pguage, "\\ ") + "+" + thouSep(patmos, "\\ ") + 
+                    "=\\underline{\\mathbf{" + thouSep(dp((pguage + patmos), 4, 2), "\\ ") + 
+                    "\\ N/m^2}}\\ (2\\ dp)\\end{aligned}$$";
+            break;
+        case 14:
+            document.getElementById("noteslink").onclick = function() {
+                window.open("images/Sci Bk2 Statics v1.10.pdf#page=67", "_blank")
+            }
+            F = rndgen(2, 3.5, 1, 0.1, -1);
+            a = rndgen(45, 75, 0, 1, -1);
+            rhofresh = 1000;
+            rhosea = 1025;
+            g = 9.81;
+            sumq += "A ship has a weight of " + F + " MN. The area at the waterline is " + a + " m<sup>2</sup>. " + 
+                    "Calculate the draught of the ship, to 2 decimal places, in:<BR>";
+            sumq += "i. Fresh water of density " + thouSep(1000, " ") + " kg/m<sup>3</sup><BR>";
+            sumq += "ii. Sea water of density " + thouSep(1025, " ") + " kg/m<sup>3</sup>";
+            suma += "\\(F=" + F + "\\times10^6\\ N\\\\\\)";
+            suma += "\\(A=" + a + "\\ m^2\\\\\\)";
+            suma += "\\(\\rho_{FRESH}=" + thouSep(rhofresh, "\\ ") + "\\ kg/m^3\\\\\\)";
+            suma += "\\(\\rho_{SEA}=" + thouSep(rhosea, "\\ ") + "\\ kg/m^3\\\\\\)";
+            suma += "\\(h_{FRESH}=\\ ?\\ m\\\\\\)";
+            suma += "\\(h_{SEA}=\\ ?\\ m\\\\\\)";
+            suma += "$$\\begin{aligned}F&=\\rho ghA\\\\[5pt]";
+            suma += "h_{FRESH}&=\\frac{F}{\\rho_{FRESH}\\ g\\ A}=\\frac{" + F + "\\times10^6}{" + 
+                    thouSep(rhofresh, "\\ ") + "\\times" + g + "\\times" + a + "}=\\underline{\\mathbf{" + 
+                    dp((F * 1000000) / (rhofresh * g * a), 4, 2) + "\\ m\\ (2\\ dp)}}\\\\[5pt]";
+            suma += "h_{SEA}&=\\frac{F}{\\rho_{SEA}\\ g\\ A}=\\frac{" + F + "\\times10^6}{" + 
+                    thouSep(rhosea, "\\ ") + "\\times" + g + "\\times" + a + "}=\\underline{\\mathbf{" + 
+                    dp((F * 1000000) / (rhosea * g * a), 4, 2) + "\\ m\\ (2\\ dp)}}\\end{aligned}$$";
+            break;
+        case 15:
+            document.getElementById("noteslink").onclick = function() {
+                window.open("images/Sci Bk2 Statics v1.10.pdf#page=67", "_blank")
+            }
+            v1 = rndgen(800, 14000, 0, 1, -1);
+            rho1 = rndgen(1.002, 1.018, 3, 0.001, -1);
+            rho2 = rndgen(1.112, 1.194, 3, 0.001, -1);
+            g = 9.81;
+            sumq += "Calculate, to 1 decimal place, the change in lift on a balloon of volume " + thouSep(v1, " ") + 
+                    " m<sup>3</sup>, when a decrease in temperature increases the air density from " + 
+                    rho1 + " kg/m<sup>3</sup> to " + rho2 + " kg/m<sup>3</sup>.";
+            suma += "\\(V=" + thouSep(v1, "\\ ") + "\\ m^3\\\\\\)";
+            suma += "\\(\\rho_1=" + rho1 + "\\ kg/m^3\\\\\\)";
+            suma += "\\(\\rho_2=" + rho2 + "\\ kg/m^3\\\\\\)";
+            suma += "$$F=\\rho ghA=\\rho gV=( \\rho_2 - \\rho_1 )gV=(" + rho2 + "-" + rho1 + 
+                    ")\\times" + g + "\\times" + thouSep(v1, "\\ ") + "=" + 
+                    thouSep(dp((rho2 - rho1) * g * v1, 3, 1), "\\ ") + "\\ N\\ (1\\ dp)$$";
             break;
     }
     document.getElementById("q").innerHTML = sumq;
