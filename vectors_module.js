d87ec1901ea129a7062b19beade3713075610dea
markb500 <81427572+markb500@users.noreply.github.com>
2021-08-04 18:20:12 +0100

    Add files via upload

--- vectors_module.js
+++ vectors_module.js
@@ -2,7 +2,7 @@ var prevsum = 0, prev2sum = 0;
 function vectors() {
     var sum;
     do {
-        sum = rndgen(1, 3, 0, 1, -1);
+        sum = rndgen(1, 4, 0, 1, -1);
     } while(sum === prevsum || sum === prev2sum)
     prev2sum = prevsum;
     prevsum = sum;
@@ -15,6 +15,9 @@ function vectors() {
             break;
         case 3:
             jetbank();
+            break;
+        case 4:
+            pinjoint();
             break;
     }
 }
